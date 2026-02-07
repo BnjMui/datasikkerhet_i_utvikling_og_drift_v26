1a9ff14407b11969fd53136d185b87624aaf2dfa
BnjMui <jonas.evensen@hotmail.com>
2026-02-07 01:05:23 +0100

    update to init.sql

--- src/init.sql
+++ src/init.sql
@@ -2,23 +2,21 @@ CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     mail VARCHAR(100) NOT NULL UNIQUE,
-    role ENUM("student", "lecturer"),
-    password VARCHAR(255) NOT NULL
+    role ENUM("student", "lecturer") NOT NULL,
+    PASSWORD VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS students (
     student_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL UNIQUE,
     study_field VARCHAR(255) NOT NULL,
     class_year YEAR NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS lecturers (
     lecturer_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL UNIQUE,
     avatar VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    FOREIGN KEY (lecturer_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS courses (
@@ -49,7 +47,7 @@ CREATE TABLE IF NOT EXISTS messages (
 
 CREATE TABLE IF NOT EXISTS replies (
     reply_id INT AUTO_INCREMENT PRIMARY KEY,
-    message_id INT NOT NULL,
+    message_id INT UNIQUE NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     text VARCHAR(255) NOT NULL,
     FOREIGN KEY (message_id) REFERENCES messages(message_id) ON DELETE CASCADE
