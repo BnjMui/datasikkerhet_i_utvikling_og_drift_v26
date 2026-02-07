36f9f73955095bd93c5f60865a52b2557297845e
BnjMui <jonas.evensen@hotmail.com>
2026-02-07 01:06:45 +0100

    db and repository class init

--- src/db.php
+++ src/db.php
@@ -1,29 +1,27 @@
 <?php
+class Database
+{
+    # Fremtidig så skal verdiene her settes når objektet initsialiseres..
+    private Pdo\Mysql $db;
+    private string $_db_host = "localhost:3306";
+    private string $_db_name = "datasikkerhet";
+    private string $_db_user = "root";
+    private string $_db_passwd = "dev";
 
-$_db_host = "localhost:3306";
-$_db_name = "datasikkerhet";
-$_db_user = "root";
-$_db_passwd = "dev";
+    public bool $connection_status;
 
-try {
-    $db = new Pdo\Mysql("mysql:host=$_db_host;dbname=$_db_name", $_db_user, $_db_passwd);
-#    echo "connection success\n";
-} catch (PDOException $e) {
-    echo "connection to db failed";
-    echo $e;
-    echo $e->getMessage();
+    public function __construct(string $host, string $db_name, string $db_user, string $db_password)
+    {
+        try {
+            $this->db = new Pdo\Mysql("mysql:host=$this->_db_host;dbname=$this->_db_name", $this->_db_user, $this->_db_passwd);
+            $this->connection_status = true;
+        } catch (PDOException $e) {
+            $this->connection_status = false;
+        }
+    }
+
+    public function getDb(): Pdo\Mysql
+    {
+        return $this->db;
+    }
 }
-
-$result = $db->query("SHOW COLUMNS FROM users;")->fetchAll();
-
-# echo json_encode($result);
-
- foreach ($result as $key => $column) {
-     echo "Column $column[Field]\n";
- }
-
-$query = null;
-$db = null;
-
-// Close connection to db, do for all variables referencing $dbh. ??
-// $db = null;
---
+++ src/repository.php
@@ -0,0 +1,124 @@
+<?php
+
+use Entities\User;
+include "./entities/user.php";
+include "db.php";
+class Repository
+{
+    private Database $db;
+    private Pdo\Mysql $dbh;
+
+    public function __construct()
+    {
+        $this->db = new Database("localhost", "datasikkerhet", "root", "dev");
+        $this->dbh = $this->db->getDb();
+    }
+
+    # Users
+    public function getUserById(int $id)
+    {
+        $dbh = $this->dbh;
+
+        $statement = $dbh->prepare("SELECT user_id, name, mail, role FROM users WHERE user_id = ?");
+        $statement->execute([$id]);
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $user = new User($result["user_id"], $result["name"], $result["mail"], $result["role"], "pass");
+        print_r($user);
+
+    }
+
+    public function createUser(User $userData)
+    {
+        $dbh = $this->dbh;
+
+        $statement = $dbh->prepare("INSERT INTO users (name, mail, role, password) VALUES (:name,:mail,:role,:password)");
+
+        $statement -> bindParam(":name", $name);
+        $statement -> bindParam(":mail", $mail);
+        $statement -> bindParam(":role", $role);
+        $statement -> bindParam(":password", $password);
+
+        $name = $userData->name;
+        $mail = $userData->mail;
+        $role = $userData->role;
+        $password = $userData->password;
+
+        $statement->execute();
+
+    }
+
+    public function createStudent() {
+    }
+
+    public function createLecturer() {
+    }
+
+    public function updatePassword()
+    {
+    }
+
+    public function getStudentById()
+    {
+    }
+
+    public function getLecturerById()
+    {
+    }
+
+    public function checkUserLoginById()
+    {
+    }
+
+    # Courses
+    public function getCourses()
+    {
+    }
+
+    public function getCourse()
+    {
+    }
+
+    public function createCourse()
+    {
+    }
+
+    public function checkCoursePin()
+    {
+    }
+
+    # Messages
+    public function getMessages()
+    {
+    }
+
+    public function createMessage()
+    {
+    }
+
+    # Responses
+    public function createResponse()
+    {
+    }
+
+    # Comments
+
+    # Reports
+
+
+
+    # TESTING
+    # $result = $db->query("SHOW COLUMNS FROM users;")->fetchAll();
+
+    # echo json_encode($result);
+
+    #  foreach ($result as $key => $column) {
+    #      echo "Column $column[Field]\n";
+    #  }
+    #
+    # $query = null;
+    # $db = null;
+
+    // Close connection to db, do for all variables referencing $dbh. ??
+    // $db = null;
+}
