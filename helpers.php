03c2d8d630232d0c5fc2b7ae797c939be033a2e0
samiisacx <samiisac93@gmail.com>
2026-02-17 11:18:30 +0100

    Update CORS settings in helpers.php

--- helpers.php
+++ helpers.php
@@ -5,14 +5,13 @@
 session_start();
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://158.39.188.223');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit(); }
 
 require_once 'Includes/db.php';
-// bytte til: // Til (skoleserveren):
 // $database = new Database('localhost', 'datasikkerhet', 'brukernavn', 'passord');
 $database = new Database('127.0.0.1', 'datasikkerhet', 'root', 'dev');
 $db = $database->getDb();
