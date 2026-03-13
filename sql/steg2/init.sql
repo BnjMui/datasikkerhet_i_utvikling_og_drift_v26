1a120d483f72db3c66ff1c9faa846fdc8a924d08
BnjMui <jonas.evensen@hotmail.com>
2026-03-13 12:15:55 +0100

    added table for security questions

---
+++ sql/steg2/init.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE IF NOT EXISTS steg2_datasikkerhet;
+
+USE steg2_datasikkerhet;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR(36) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    mail VARCHAR(100) NOT NULL UNIQUE,
+    role ENUM("student", "lecturer") NOT NULL,
+    PASSWORD VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS students (
+    student_id VARCHAR(36) PRIMARY KEY,
+    study_field VARCHAR(255) NOT NULL,
+    class_year YEAR NOT NULL,
+    ADD
+        CONSTRAINT FK_student_id FOREIGN KEY (student_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS lecturers (
+    lecturer_id VARCHAR(36) PRIMARY KEY,
+    avatar VARCHAR(255),
+    ADD
+        CONSTRAINT FK_lecturerId FOREIGN KEY (lecturer_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS security_questions (
+    question_id VARCHAR(36) PRIMARY KEY,
+    user_id VARCHAR(36) NOT NULL,
+    security_question VARCHAR(255) NOT NULL,
+    security_answer VARCHAR(255) NOT NULL,
+    ADD
+        CONSTRAINT FK_securityQuestion FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    lecturer_id VARCHAR(36) NOT NULL,
+    course_code VARCHAR(14) NOT NULL UNIQUE,
+    course_name VARCHAR(50) NOT NULL,
+    pin_code CHAR(4),
+    ADD
+        CONSTRAINT FK_lecturerCourse FOREIGN KEY (lecturer_id) REFERENCES lecturers(lecturer_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS students_courses (
+    student_id VARCHAR(36) NOT NULL,
+    course_id INT NOT NULL,
+    PRIMARY KEY (student_id, course_id),
+    ADD
+        CONSTRAINT FK_course_StudentId FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
+    ADD
+        CONSTRAINT FK_student_courseId FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id VARCHAR(36) NOT NULL,
+    course_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR(255) NOT NULL,
+    ADD
+        CONSTRAINT FK_studentMessage FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
+    ADD
+        CONSTRAINT FK_courseMessage FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS replies (
+    reply_id INT AUTO_INCREMENT PRIMARY KEY,
+    message_id INT UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR(255) NOT NULL,
+    ADD
+        CONSTRAINT FK_messageReply FOREIGN KEY (message_id) REFERENCES messages(message_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    message_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR(255) NOT NULL,
+    ADD
+        CONSTRAINT FK_messageComment FOREIGN KEY (message_id) REFERENCES messages(message_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS reports (
+    report_id INT AUTO_INCREMENT PRIMARY KEY,
+    message_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR (255) NOT NULL,
+    ADD
+        CONSTRAINT FK_message_report FOREIGN KEY (message_id) REFERENCES messages(message_id) ON DELETE CASCADE
+);
