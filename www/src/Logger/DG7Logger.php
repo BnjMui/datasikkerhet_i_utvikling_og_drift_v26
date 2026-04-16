595eff58fcf9a59f4189b265efb550be309261ef
BnjMui <jonas.evensen@hotmail.com>
2026-04-16 20:45:35 +0200

    Configured monolog to use gelf for graylog

--- www/src/Logger/DG7Logger.php
+++ www/src/Logger/DG7Logger.php
@@ -4,9 +4,11 @@ namespace DatasikkerhetG7\Logger;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+use Gelf\Publisher;
+use Gelf\Transport\UdpTransport;
+use Monolog\Handler\GelfHandler;
 use Monolog\Level;
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 use Psr\Log\LoggerInterface;
 
@@ -23,11 +25,17 @@ class DG7Logger
         $this->logger = new Logger($name);
 
         # Push/Stream handler
-        $this->logger->pushHandler(new StreamHandler('php://stdout', Level::Info));
+        #$this->logger->pushHandler(new StreamHandler('php://stdout', Level::Info));
+
+        $transport = new UdpTransport("158.39.288.228", 12201, UdpTransport::CHUNK_SIZE_LAN);
+        $publisher = new Publisher();
+        $publisher->addTransport($transport);
+
+
+        $this->logger->pushHandler(new GelfHandler($publisher, Level::Info));
 
 
         # Other settings?
-        
     }
 
     public function getLogger(): LoggerInterface {
